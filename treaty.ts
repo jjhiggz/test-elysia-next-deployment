fe0bb70ac552336c37bbfed5aae49e40a980f484
jjhiggz <jonathan.higger@gmail.com>
2024-02-06 16:54:04 -0700

    adding some logging to see where I'm treatying too

--- treaty.ts
+++ treaty.ts
@@ -5,4 +5,5 @@ const url = process.env.VERCEL_URL
   ? "http://" + process.env.VERCEL_URL
   : "http://localhost:3000";
 
+console.log(`setting up eden treaty to ${url}`);
 export const treaty = edenTreaty<MyElysiaApp>(url);
