430c57354f1f92b785531f74c74bbfaa9062cc6b
jjhiggz <jonathan.higger@gmail.com>
2024-02-06 16:50:06 -0700

    yay

---
+++ src/app/component/Dogs.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { treaty } from "../../../treaty";
+
+export const Dogs = () => {
+  const [dogs, setDogs] = useState<string[]>();
+
+  useEffect(() => {
+    treaty.api.elysia.dogs.get().then(({ data }) => {
+      if (data) {
+        setDogs(data);
+      }
+    });
+  }, []);
+  return (
+    <ol>
+      {dogs?.map((dog) => (
+        <li key={dog}>{dog}</li>
+      ))}
+    </ol>
+  );
+};
