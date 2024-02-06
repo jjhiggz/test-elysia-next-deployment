068000d726ca31a1f7811ba38f6468bf781e0c66
jjhiggz <jonathan.higger@gmail.com>
2024-02-06 16:59:00 -0700

    trying the main deployment url for the treaty

--- treaty.ts
+++ treaty.ts
@@ -2,8 +2,7 @@ import { MyElysiaApp } from "@/app/api/elysia/[...slugs]/route";
 import { edenTreaty } from "@elysiajs/eden";
 
 const url = process.env.VERCEL_URL
-  ? "http://" + process.env.VERCEL_URL
+  ? "https://test-elysia-next-deployment.vercel.app/"
   : "http://localhost:3000";
 
-console.log(`setting up eden treaty to ${url}`);
-export const treaty = edenTreaty<MyElysiaApp>(url);
+export const treaty = edenTreaty<MyElysiaApp>(url, {});
